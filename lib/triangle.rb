db87d6ae431bc252b02b549e28afd061d8ff8ba7
Reginald Jean Amedee <reginald.amedee@gmail.com>
2019-06-21 19:14:00 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,7 +12,6 @@ def kind
   if (side1 * side2 * side3) == 0 || (side1 + side2) <= side3 || (side2 + side3) <= side1 || (side3 + side1) <= side2
     begin
       raise TriangleError
-        puts error.message
         
     elsif side1 == side2 && side3 || side2 == side1 && side3 || side3 == side1 && side2
       self.kind = :equilateral
