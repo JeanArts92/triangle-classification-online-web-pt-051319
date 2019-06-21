3f14d200818fb77847be55755cbc468fce2b0fde
Reginald Jean Amedee <reginald.amedee@gmail.com>
2019-06-21 19:11:35 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -17,7 +17,8 @@ def kind
     elsif side1 == side2 && side3 || side2 == side1 && side3 || side3 == side1 && side2
       self.kind = :equilateral
     elsif side1 != side2 && side3 || side2 != side1 && side3 || side3 != side1 && side2 
-      self.kind = :isos
+      self.kind = :scalene 
+    else 
 
     
 end 
