fd898305e051c66b1a5dbd42d33c8b01ddd51668
Reginald Jean Amedee <reginald.amedee@gmail.com>
2019-06-21 19:22:02 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -16,7 +16,7 @@ def kind
       puts error.message
     end 
         
-    elsif side1 == side2 && side3 || side2 == side1 && side3 || side3 == side1 && side2
+    elsif side1 == side2 && side1 == side3
       :equilateral
     elsif side1 != side2 && side3 || side2 != side1 && side3 || side3 != side1 && side2 
       :scalene 
