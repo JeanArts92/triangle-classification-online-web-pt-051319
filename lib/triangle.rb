ecc1dc8747b8ab1f12662ad04198b44df3a2de94
Reginald Jean Amedee <reginald.amedee@gmail.com>
2019-06-21 19:22:57 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -19,7 +19,7 @@ def kind
     elsif side1 == side2 && side1 == side3
       :equilateral
     elsif side1 == side2 || side1 == side3 || side2 == side3
-      :isoscelese
+      :isosceles
     else 
       :scalene
     end 
